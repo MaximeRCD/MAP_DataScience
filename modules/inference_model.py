dfe218ea1601d51c4ed5af58070ee483f0bbb526
maxime richaudeau <maxime.richaudeau1@gmail.com>
2024-02-19 15:49:14 +0100

    inference_model.py passed in pylint and black formatter

--- modules/inference_model.py
+++ modules/inference_model.py
@@ -1,31 +1,48 @@
-from collections import defaultdict
-from tqdm import tqdm
-import torch.backends.cudnn as cudnn
-import torch.nn as nn
+"""
+This module contains functions for loading a pre-trained model and making predictions on a dataset
+of images for the task of detecting free parking spaces. It utilizes a U-Net model architecture
+from the ternausnet library, processes images using albumentations for transformations, and employs
+PyTorch for model operations.
+"""
+
 import torch.optim
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 import ternausnet.models
-import os
-import cv2
 from torch.utils.data import DataLoader
 import torch
-import matplotlib.pyplot as plt
-import numpy as np
-from .constants import PARAMS, TEST_IMAGE_DIR
-from .datasets import FreeParkingPlacesInferenceDataset
+from .constants import PARAMS
+
 
 def load_model(model_file_path):
+    """
+    Loads a model from a specified file path.
+
+    Args:
+        model_file_path (str): Path to the model file.
+
+    Returns:
+        torch.nn.Module: The loaded model.
+    """
     model = getattr(ternausnet.models, "UNet11")(pretrained=False)
-    model.load_state_dict(torch.load(model_file_path, map_location=PARAMS['device']))
-    model = model.to(PARAMS['device'])
+    model.load_state_dict(torch.load(model_file_path, map_location=PARAMS["device"]))
+    model = model.to(PARAMS["device"])
     return model
 
+
 def predict(model, params, test_dataset, batch_size):
+    """
+    Generates predictions for a given dataset using the specified model.
+
+    Args:
+        model (torch.nn.Module): The model to use for predictions.
+        params (dict): Parameters for model and device settings.
+        test_dataset (Dataset): The dataset to predict on.
+        batch_size (int): The batch size for processing.
+
+    Returns:
+        list: A list of tuples containing predicted masks and their original dimensions.
+    """
     # Initialize DataLoader for the test dataset
-    test_loader = DataLoader(
-        test_dataset, batch_size=batch_size, shuffle=False,
-    )
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     model.eval()  # Set the model to evaluation mode
     predictions = []
