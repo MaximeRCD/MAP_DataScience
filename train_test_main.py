af32ca671cac6035e21b9db91728436a65db19d1
maxime richaudeau <maxime.richaudeau1@gmail.com>
2024-02-19 14:54:47 +0100

    start testing script version of the code

---
+++ train_test_main.py
@@ -0,0 +1,54 @@
+import os
+import cv2
+from torch.utils.data import Dataset, DataLoader, random_split
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import ternausnet.models
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim
+from tqdm import tqdm
+from modules.utils import Visualizer, preprocess_mask, check_cuda_availability
+from modules.constants import *
+from modules.datasets import FreeParkingPlacesDataset, FreeParkingPlacesInferenceDataset
+## 2. Configuration de l'environnement
+### 2.1 Test de la disponibilité d'un GPU compatible avec Pytorch
+
+### 2.2 Définition de classes utilitaires
+
+### 2.3 Définition des classes afin de créer les datasets
+### 2.4 Constants & Helper Entities definitions
+visualizer_worker = Visualizer()
+
+TEST_IMAGE_FILENAMES = os.listdir(TEST_IMAGE_DIR)
+
+### 2.5 Definition of Training & Validation DataSets
+training_ds = FreeParkingPlacesDataset(
+    images_directory=TRAINING_IMAGE_DIR,
+    masks_directory=TEST_MASK_DIR,
+    transform=A.Compose([
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
+        ToTensorV2()])
+)
+
+validation_ds = FreeParkingPlacesDataset(
+    images_directory=VAL_IMAGE_DIR,
+    masks_directory=VAL_MASK_DIR,
+    transform=A.Compose([
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
+        ToTensorV2()])
+)
+
+train_loader = DataLoader(training_ds, batch_size=VISUALIZER_BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(validation_ds, batch_size=VISUALIZER_BATCH_SIZE, shuffle=False)
+#### Visualisation des données dans le DataSet de Training
+#Pour le masque, nous avons décidé d'afficher également le pourcentage de pixels représentant les places de parking pour avoir une idée de la proportion de labels 1 et 0.
+visualizer_worker.visualize_batch(train_loader)
+#### Visualisation des données dans le DataSet de Validation
+#Pour le masque, nous avons également décidé d'afficher le pourcentage de pixels représentant les places de parking pour avoir une idée de la proportion de labels 1 et 0.
+visualizer_worker.visualize_batch(val_loader)
+#### Visualisation des données dans le DataSet de Test
+visualizer_worker.display_image_grid(TEST_IMAGE_FILENAMES, TEST_IMAGE_DIR, TEST_MASK_DIR)
